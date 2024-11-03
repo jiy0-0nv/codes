1908c841f83992a82900629e788535373e2bba21
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2024-11-03 19:13:18 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 720 ms, Memory: 64444 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,31 @@
+import queue
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited = [0 for _ in range(n + 1)]
+q = queue.Queue()
+
+# bfs
+order = 1
+visited[r] = order
+q.put(r)
+
+while q.qsize() > 0:
+    p = q.get()
+    graph[p].sort(reverse=True)
+    for np in graph[p]:
+        if visited[np] == 0:
+            order += 1
+            visited[np] = order
+            q.put(np)
+
+for i in range(1, n + 1):
+    print(visited[i])
