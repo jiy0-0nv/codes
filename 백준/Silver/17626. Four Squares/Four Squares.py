bc448ceb5308c2afb59deb6083ee7ba36ddd789f
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2024-12-22 19:51:47 +0900

    [Silver III] Title: Four Squares, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,15 +1,20 @@
 n = int(input())
-
-dp = [n] * (n + 1)
-dp[0] = 0
+m = [False] * 50001
 
 for i in range(1, int(n ** (1/2) + 1)):
-    dp[i ** 2] = 1
-for i in range(1, n + 1):
-    if dp[i] > 4:
-        for j in range(1, int(i ** (1/2) + 1)):
-            if dp[i] < 3:
-                break
-            dp[i] = min(dp[i], dp[i - j ** 2] + 1)
+    m[i ** 2] = True
 
-print(dp[n])
+ans = 4
+for i in range(int(n ** 0.5), 0, -1):
+    if m[n]:
+        ans = 1
+        break
+    if m[n - i ** 2]:
+        ans = 2
+        break
+    else:
+        for j in range(int((n - i ** 2) ** (1/2) + 1), 0, -1):
+            if m[n - i ** 2 - j ** 2]:
+                ans = 3
+
+print(ans)
