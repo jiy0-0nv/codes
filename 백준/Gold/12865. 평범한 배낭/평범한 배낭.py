6818807eb05ae84f019239668a2e3f4b9b579549
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-01-27 11:19:48 +0900

    [Gold V] Title: 평범한 배낭, Time: 2456 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,20 +3,13 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 stuffs = [0] * n
-# dp = [[0] * (k + 1) for _ in range(n + 1)]
 dp = [0] * (k + 1)
 
 for i in range(n):
     stuffs[i] = tuple(map(int, input().split()))
 
-for i in range(1, n + 1):
-    ndp = [0] * (k + 1)
-    for j in range(1, k + 1):
-        if j < stuffs[i - 1][0]:
-            ndp[j] = dp[j]
-        else:
-            ndp[j] = max(dp[j], dp[j - stuffs[i - 1][0]] + stuffs[i - 1][1])
-    for i in range(k + 1):
-        dp[i] = ndp[i]
+for w, v in stuffs:
+    for i in range(k, w - 1, -1):
+        dp[i] = max(dp[i], dp[i - w] + v)
 
 print(dp[k])
