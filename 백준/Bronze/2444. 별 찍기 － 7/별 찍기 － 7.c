b4e7e3d8bba6285b72862a42787f13052206026c
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-03-10 22:41:57 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int stars(int i, int n){
-    for(int j=0; j<n-1-i; j++){
-        printf(" ");
-    }
-    for(int j=0; j<i*2+1; j++){
-        printf("*");
-    }
+    for(int j=0; j<n-1-i; j++)printf(" ");
+    for(int j=0; j<i*2+1; j++)printf("*");
     printf("\n");
 }
 
@@ -14,11 +10,7 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    for(int i=0; i<n; i++){
-        stars(i, n);
-    }
-    for(int i=n-2; i>=0; i--){
-        stars(i, n);
-    }
+    for(int i=0; i<n; i++)stars(i, n);
+    for(int i=n-2; i>=0; i--)stars(i, n);
     return 0;
 }
