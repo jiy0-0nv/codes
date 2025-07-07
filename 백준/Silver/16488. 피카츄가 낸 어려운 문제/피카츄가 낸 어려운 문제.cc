28cc8e8f0f010cd8d984442d11f71c60e41b52e9
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-07-07 20:23:52 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n, k;
+    cin>>n>>k;
+    cout<<k*n*n;
+
+    return 0;
+}
