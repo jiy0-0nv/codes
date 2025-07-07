f9efd47748fe21f0bb9a54231cd9f9fc8a973678
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-07-07 15:29:44 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    cout<<n-2024;
+
+    return 0;
+}
