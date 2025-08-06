114e1869ca90d8d66bbe42b6e9e231dd990e8ee2
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-08-06 17:51:03 +0900

    가장 긴 증가하는 부분 수열 4

---
+++ 백준/Gold/1002.cc
@@ -0,0 +1,41 @@
+#if 01
+#include <iostream>
+
+using namespace std;
+
+int N;
+int nlst[1001] = { 0, }; // 1~
+int dp[1001] = {0, 1, };
+int pv[1001] = {};
+int ansidx = 0;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> nlst[i];
+		for (int j = 1; j < i; j++) {
+			if (nlst[j] < nlst[i] && dp[j] + 1 > dp[i]) {
+				dp[i] = dp[j] + 1;
+				pv[i] = j;
+			}
+		}
+		if (dp[i] > dp[ansidx]) {
+			ansidx = i;
+		}
+	}
+	cout << dp[ansidx] << "\n";
+
+	int ans[1001];
+	int t = ansidx;
+	for (int i = ansidx; i > 0; i--) {
+		ans[i] = nlst[t];
+		t = pv[i];
+	}
+	for (int i = 1; i <= ansidx; i++) cout << ans[i] << " ";
+
+	return 0;
+}
+#endif
