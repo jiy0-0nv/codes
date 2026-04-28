c1a9ed880557b1868beb0f55a909280013f095c6
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2026-04-28 20:54:54 +0900

    [level 2] Title: 소수 찾기, Time: 79.31 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int nums[10];
+bool np[10000000];
+
+bool canMake(int num) {
+    int n = num;
+    
+    int ns[10];
+    for (int i=0; i<10; i++)
+        ns[i] = nums[i];
+    
+    while (n) {
+        int t = n % 10;
+        if (ns[t]) {
+            ns[t]--;
+            n /= 10;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    
+    fill(nums, nums+10, 0);
+    for (char n : numbers)
+        nums[n - '0']++;
+    
+    fill(np, np+10000000, false);
+    for (int i=2; i<5000000; i++) {
+        if (np[i]==false) {
+            if (canMake(i)) answer++;
+            for (int j=i; j<10000000; j+=i) {
+                np[j] = true;
+            }
+        }
+    }
+    
+    return answer;
+}
